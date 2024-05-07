24dd200c5cd8168aa6d8842b7099c5319cc78c6c
Kodis0 <ekimta110@gmail.com>
2024-05-07 15:46:12 +0300

    Commit Изменено название и прописаны настройки api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import React, {useState, useEffect} from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    fetch('/api')
+    .then(response => response.json())
+    .then(response => setData(response.message))
+  }, [])
+
   return (
     <div className="RegistryPage">
       <header className="RegistryPage-header">
         <img src={logo} className="RegistryPage-logo" alt="logo" />
+
+        <p>
+          {
+            !data ? "Loading..." : data
+          }
+        </p>
+
         <div className='h1'>
-          <h1>Telegram</h1>
+          <h1>Monogram</h1>
           </div>
           <div>
           <text className='ConfirmText'>Please select your country and enter your number</text>

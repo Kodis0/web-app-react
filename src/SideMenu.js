956dcd8e03a1e9d2842a9bacb74363872fc46d55
Kodis0 <ekimta110@gmail.com>
2024-05-20 15:48:21 +0300

    Добавление main страницы

---
+++ src/SideMenu.js
@@ -0,0 +1,17 @@
+// SideMenu.js
+import React from 'react';
+import './SideMenu.css';
+
+function SideMenu() {
+  return (
+    <div className="side-menu">
+      <ul>
+        <li><a href="#">Settings 1</a></li>
+        <li><a href="#">Settings 2</a></li>
+        <li><a href="#">Settings 3</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default SideMenu;
--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
+// MainPage.js
 import React from 'react';
+import SideMenu from './SideMenu';
+import './Main.css';
 
 function MainPage() {
   return (
-    <div>
-      <h1>Main Page</h1>
+    <div className="main-page">
+      <SideMenu />
+      <div className="main-content">
+        {/* Здесь можете разместить основное содержимое страницы */}
+      </div>
     </div>
   );
 }
